ad54fb7fe367512d3cd9f32db0fe3905b5ca2a43
Gergely Dezso <gergelydezso.gd@gmail.com>
2013-03-18 23:40:38 +0200

    up

---
+++ ArduinoProgram/ArduinoProgram.ino
@@ -0,0 +1,25 @@
+#include <Servo.h> 
+ 
+Servo myservo; 
+               
+int pos = 0;    
+int incomingByte = 0;
+ 
+void setup() 
+{ 
+  Serial.begin(9600); 
+  myservo.attach(9);  
+} 
+void loop() 
+{                                 
+    if (Serial.available() > 0) {
+     
+      char c1 = Serial.read();
+      pos = c1 - '0';
+      Serial.print(pos);
+      myservo.write(pos*10);             
+      delay(20);                     
+    
+  }
+ 
+} 
