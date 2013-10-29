47600eff9a449f904d970a236ca16b06a8617d30
Andrei Hegedus-Sirbu <andrei.hegedus-sirbu@iquestgroup.com>
2013-10-29 17:45:20 +0200

    Added code review notes and comments.

--- SmartLampSDK/src/com/gergelydezso/smartlampsdk/connection/BridgeBuildCallback.java
+++ SmartLampSDK/src/com/gergelydezso/smartlampsdk/connection/BridgeBuildCallback.java
@@ -1,5 +1,6 @@
 package com.gergelydezso.smartlampsdk.connection;
 
+// TODO - CODE REVIEW - andrei|Oct 29, 2013 - BridgeCreationListener
 public interface BridgeBuildCallback {
 
   public void onConnectionReady(SmartLampCommunicationBridge bridge);
--- SmartLampSample/src/com/gergelydezso/smartlampsdk/sampleapp/SmartLampSampleApplication.java
+++ SmartLampSample/src/com/gergelydezso/smartlampsdk/sampleapp/SmartLampSampleApplication.java
@@ -2,6 +2,7 @@ package com.gergelydezso.smartlampsdk.sampleapp;
 
 import android.app.Application;
 
+import com.gergelydezso.smartlampsdk.api.SmartLampAPI;
 import com.gergelydezso.smartlampsdk.sampleapp.notifications.DeviceNotifications;
 
 public class SmartLampSampleApplication extends Application {
@@ -9,7 +10,6 @@ public class SmartLampSampleApplication extends Application {
   @Override
   public void onCreate() {
     DeviceNotifications.init(getApplicationContext());
-
   }
 
 }
